663a46377e2da5a0411e1c96855a6013b5c6564f
victor <victor@sahar.fr>
2025-12-10 15:53:37 +0100

    fix import issue

--- lago/__init__.py
+++ lago/__init__.py
@@ -1,4 +1,11 @@
-from lago.l_modularity_function import longitudinal_modularity
-from lago.lago import lago_communities
-from lago.linkstream import LinkStream
-from lago.plot import plot_dynamic_communities
+from .l_modularity_function import longitudinal_modularity
+from .lago import lago_communities
+from .linkstream import LinkStream
+from .plot import plot_dynamic_communities
+
+__all__ = [
+    "longitudinal_modularity",
+    "LinkStream",
+    "lago_communities",
+    "plot_dynamic_communities",
+]
